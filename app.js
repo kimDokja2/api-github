1a2eb3c72b1aafc0b66b519013d2916ce66c5e4e
kimDokja2 <sucaronaldota@gmail.com>
2022-10-27 17:16:42 -0500

    myUrl

--- app.js
+++ app.js
@@ -1,62 +1,84 @@
-document.getElementById('btn-search').addEventListener('click',showGithubUser)
+const myUrl = "https://api.github.com/users/kimDokja2";
 
-function showGithubUser(){
-    let user= document.getElementById('usergithub').value
-
-    let url= 'https://api.github.com/users/'+user
-    fetch(url).then(res=>res.json()).then(data=>{
-        if(data.message){
-            alert('No existe el usuario '+user)
-
-        }else{
-            console.log(data)
-            document.getElementById('imagen').innerHTML=`
-                <img width="100" src="${data.avatar_url}"></img>
-            `
-            document.getElementById('name').innerHTML=`
-            <div>${data.name}</div>
-            `
-
-            const dias=data.created_at
-            const fecha=dias.slice(0, -10)
-
-            const fechaFormato = new Date(fecha)
-            const opciones = {  month: 'short',year: 'numeric', day: 'numeric' }
-            const fechaCreada=fechaFormato.toLocaleDateString('es-PE', opciones)
-
-            document.getElementById('created-at').innerHTML=`
-                <div class="joined">Joined ${fechaCreada}</div>
-            `
-            document.getElementById('login').innerHTML=`
-            <div>@${data.login}</div>
-            `
-            document.getElementById('bio').innerHTML=`
-            <div>@${data.bio}</div>
-            `
-            document.getElementById('public_repos').innerHTML=`
-            <div>${data.public_repos}</div>
-            `
-            document.getElementById('followers').innerHTML=`
-            <div>${data.followers}</div>
-            `
-            document.getElementById('following').innerHTML=`
-            <div>${data.following}</div>
-            `
-            document.getElementById('location').innerHTML=`
-            <div><img width="15" src="./image/pin.png" alt="" />&nbsp;${data.location}</div>
-            `
-            document.getElementById('blog').innerHTML=`
-            <div><img width="15" src="./image/link.png" alt="" />&nbsp;${data.blog}</div>
-            `
-            document.getElementById('twitter_username').innerHTML=`
-            <div><img width="15" src="./image/twitter.png" alt="" />&nbsp;${data.twitter_username}</div>
-            `
-            document.getElementById('company').innerHTML=`
-            <div><img width="15" src="./image/hotel.png" alt="" />&nbsp;${data.company}</div>
-            `
-        }
-        
-    }).catch(e=>{
-        console.log("error "+e)
-    })
+async function loadUser(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    renderUser(data);
+  } catch (error) {
+    console.log("error", error);
+  }
 }
+function renderUser(data) {
+  const fecha = data.created_at.slice(0, -10);
+  const fechaFormato = new Date(fecha);
+  const opciones = { month: "long", year: "numeric", day: "numeric" };
+  const fechaCreada = fechaFormato.toLocaleDateString("en-US", opciones);
+  document.getElementById("container").innerHTML = `
+        <!-- Imagen de perfil -->
+        <div class="image-container" id="imagen">
+          <img
+            width="100"
+            src="${data.avatar_url}"
+            alt=""
+          />
+        </div>
+        <!-- Description del perfil -->
+        <div class="description-container">
+          <!-- Nombre de usuario y año de union -->
+          <div class="user-date-container">
+            <h2 id="name">${data.name}</h2>
+            <p id="created-at">Joined ${fechaCreada}</p>
+          </div>
+          <!-- Nombre de usuario y la bio -->
+          <div class="user-bio-container">
+            <p id="login">@${data.login}</p>
+            <p id="bio">${data.bio}</p>
+          </div>
+          <!-- Card con los repos, followes, following -->
+          <div class="card-information">
+            <div>
+              <h5>Repos</h5>
+              <h2 id="public_repos">${data.public_repos}</h2>
+            </div>
+            <div>
+              <h5>Followers</h5>
+              <h2 id="followers">${data.followers}</h2>
+            </div>
+            <div>
+              <h5>Following</h5>
+              <h2 id="following">${data.following}</h2>
+            </div>
+          </div>
+          <!-- Info de ubi, twitter, link, username -->
+          <div class="info-container">
+            <div>
+              <p id="location">
+                <img width="15" src="./images/pin.png" alt="" />&nbsp; ${data.location}
+              </p>
+              <p id="blog">
+                <img width="15" src="./images/link.png" alt="" />&nbsp; ${data.blog}
+              </p>
+            </div>
+            <div>
+              <p id="twitter_username">
+                <img width="15" src="./images/twitter.png" alt="" />&nbsp; ${data.twitter_username}
+              </p>
+
+              <p id="company">
+                <img width="15" src="./images/hotel.png" alt="" />&nbsp; ${data.company}
+              </p>
+            </div>
+          </div>
+        </div>
+    `;
+}
+
+document.getElementById("btn-search").addEventListener("click", (e) => {
+  let user = document.getElementById("usergithub").value;
+  let url = "https://api.github.com/users/" + user;
+  loadUser(url);
+});
+
+loadUser(myUrl);
